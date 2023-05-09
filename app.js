b29a82be52de76e4aab617db4c102ac59ea31143
Anuj0124Kumar <anuj0214@gmail.com>
2023-05-09 09:51:34 +0530

    checkAppFile

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const app = express();
+const vhost = require('vhost');
 
-const app = express ();
 
-app.get('/',(req,res) =>{
-    res.send('main Domain')
-})
 
-app.get('/example1',(req,res) =>{
-    res.send('main Domain')
-})
+// Create subdomain dynamically
+app.use('/:subdomain', (req, res, next) => {
+  const subdomain = req.params.subdomain;
+  const router = express.Router();
+  router.get('/', (req, res) => {
+    res.send(`Hello from subdomain ${subdomain}`);
+  });
+  app.use(vhost(`${subdomain}.localhost:3000`, router));
+  next();
+});
 
-app.listen(3000,()=>{
-    console.log("Server started on port 3000");
-})
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+
